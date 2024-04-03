b121fd7641d75c0fb63f83db8bf4aded03fabaa3
Gaurav8055 <gauravkadam837@gmail.com>
2024-04-03 10:35:55 +0530

    add new files

--- logger/src/main/java/com/example/demo/controller/Logger2Controller.java
+++ logger/src/main/java/com/example/demo/controller/Logger2Controller.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
-public class Logger2Controller {
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping
+public class Logger2Controller {
+	public String get()
+	{
+		System.out.println("Hello");
+		
+		return get() ;
+	}
 }
